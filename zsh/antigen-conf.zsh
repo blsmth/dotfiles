3c68b9c7fb165980eb90f21a89552837f43d8500
blsmth <brendan@thronegroup.com>
2015-07-02 17:14:10 -0400

    moving antigen conf to appropriate files

---
+++ zsh/antigen-conf.zsh
@@ -0,0 +1,8 @@
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle git
+antigen bundle pip
+antigen bundle kennethreitz/autoenv
+antigen use oh-my-zsh
+antigen theme robbyrussell
+
+antigen apply
